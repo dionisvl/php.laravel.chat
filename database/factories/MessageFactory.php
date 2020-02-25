02b6dd62b58da3cb339d79b11a69d53a17d359e5
den <dden_vlad@mail.ru>
2020-02-25 21:15:32 +0300

    add README.md and MessageFactory

--- database/factories/MessageFactory.php
+++ database/factories/MessageFactory.php
@@ -6,7 +6,9 @@ use App\Message;
 use Faker\Generator as Faker;
 
 $factory->define(Message::class, function (Faker $faker) {
+
     return [
-        //
+        'user_name' => $faker->name,
+        'msg' => $faker->realText(80),
     ];
 });
